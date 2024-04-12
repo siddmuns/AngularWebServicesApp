8bb9c8e2b54fef389e9d3b64e77605139bc263ac
Munsif Siddiqui <siddmuns@sheridancollege.ca>
2024-04-11 22:06:26 -0400

    Initial Commit

---
+++ src/main/java/sheridancollege/siddmuns/a4_webservices/A4WebServicesApplication.java
@@ -0,0 +1,13 @@
+package sheridancollege.siddmuns.a4_webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A4WebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A4WebServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridancollege/siddmuns/a4_webservices/A4WebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridancollege.siddmuns.a4_webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A4WebServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
